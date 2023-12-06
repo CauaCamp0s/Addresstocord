a0db6ad76b3a25ed7327bdfddaa4f24688337498
Cauã Campos <116642309+CauaCamp0s@users.noreply.github.com>
2023-12-06 12:08:17 -0300

    Update main.py
    Código atualizado

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import time
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,12 +10,14 @@ import pandas as pd
 from selenium.webdriver.common.by import By
 
 class WebScraper(object):
-    def __init__(self):
+    def __init__(self, excel_file_path):
         self.driver = webdriver.Firefox()
+        self.excel_file_path = excel_file_path
 
     def pesquisar_latitude_longitude(self, endereco):
-        self.driver.get("https://maps.google.com/")
-        input_element = self.driver.find_element(By.NAME, 'q')
+        driver = self.driver
+        driver.get("https://maps.google.com/")
+        input_element = driver.find_element(By.NAME, 'q')
 
         if input_element is None:
             raise Exception('Volte e tente de novo')
@@ -21,43 +25,63 @@ class WebScraper(object):
         input_element.send_keys(endereco)
         input_element.send_keys(Keys.ENTER)
 
-        WebDriverWait(self.driver, 10).until(EC.url_contains('/@'))
-        url = self.driver.current_url
+        WebDriverWait(driver, 10).until(EC.url_contains('/@'))
+        url = driver.current_url
 
-        # Extract latitude and longitude from the URL
+        # Extrair latitude e longitude da URL
         latitude, longitude = re.findall(r'@(-?\d+\.\d+),(-?\d+\.\d+)', url)[0]
         return float(latitude), float(longitude)
 
-    def buscar_coordenadas_excel(self, caminho_arquivo):
-        df = pd.read_excel(caminho_arquivo)
+    def buscar_coordenadas_excel(self):
+        df = pd.read_excel(self.excel_file_path)
 
         df['Latitude'] = None
         df['Longitude'] = None
 
-        for indice, linha in df.iterrows():
-            endereco = linha['Endereco']  # Substitua 'Endereco' pelo nome da coluna que contém os endereços
+        for i, linha in df.iterrows():
+            endereco = linha['Endereço Completo']
+
             latitude, longitude = self.pesquisar_latitude_longitude(endereco)
-            df.at[indice, 'Latitude'] = latitude
-            df.at[indice, 'Longitude'] = longitude
+            df.at[i, 'Latitude'] = latitude
+            df.at[i, 'Longitude'] = longitude
 
         return df
 
     def fechar_driver(self):
         self.driver.quit()
 
+def processar_excel(excel_file):
+    web_scraper = WebScraper(excel_file)
+    df_resultado = web_scraper.buscar_coordenadas_excel()
+    output_file_path = excel_file.replace(".xlsx", "_saida.xlsx")
+    df_resultado.to_excel(output_file_path, index=False)
+    web_scraper.fechar_driver()
+    return output_file_path  # Retorna o caminho do arquivo de saída
+
+# Lista de caminhos dos arquivos Excel
+excel_files = [
+    r"C:\Users\caua.campos\Desktop\novosProjetos\XLSX\novo.xlsx",
+]
+
 # Medir o tempo de execução
 start_time = time.time()
 
-# Exemplo de uso
-web_scraper = WebScraper()
-caminho_arquivo_excel = "C:\\Users\\caua.campos\\Desktop\\web-scrapper\\seuarquivo_modificado.xlsx"
-df_resultado = web_scraper.buscar_coordenadas_excel(caminho_arquivo_excel)
-
-# Salvar o DataFrame resultante em um novo arquivo Excel
-df_resultado.to_excel("C:\\Users\\caua.campos\\Desktop\\web-scrapper\\coordenadas_resultado.xlsx", index=False)
-
-web_scraper.fechar_driver()
+# Executar em paralelo usando threads
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    output_paths = list(executor.map(processar_excel, excel_files))
 
 end_time = time.time()
 tempo_total = end_time - start_time
 print(f"Tempo total de execução: {tempo_total} segundos")
+
+# Juntar os resultados em um único DataFrame
+dfs = [pd.read_excel(output_path) for output_path in output_paths]
+df_final = pd.concat(dfs, ignore_index=True)
+
+# Especificar o caminho completo para o arquivo Excel final
+output_final_file_path = r"C:\Users\caua.campos\Desktop\novosProjetos\XLSX\resultado_final.xlsx"
+
+# Salvar o DataFrame resultante em um novo arquivo Excel
+df_final.to_excel(output_final_file_path, index=False)
+
+print(f"Arquivo final salvo em: {output_final_file_path}")
